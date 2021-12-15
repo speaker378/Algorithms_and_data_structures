1e2a404f8db4f7486cde7f5bd1ed2d1c3d7a234c
speaker378 <speaker378debua@gmail.com>
2021-12-15 18:57:41 +0500

    Initial commit

---
+++ Algorithms_and_data_structures/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Algorithms_and_data_structures
+//
+//  Created by Сергей Черных on 15.12.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Algorithms_and_data_structures
+ Homework within the course from GeekBrains
