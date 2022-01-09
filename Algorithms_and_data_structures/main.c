b03fb19bd627bdd75afd13df2f7c217f69528984
speaker378 <speaker378debua@gmail.com>
2022-01-09 14:54:20 +0500

    task 2

--- Algorithms_and_data_structures/main.c
+++ Algorithms_and_data_structures/main.c
@@ -15,15 +15,14 @@
 
 
 #include <stdio.h>
-#define row 5
-#define column 5
+#include <string.h>
 
-void print (int n, int m, int a[row][column])
+void print (int row, int column, int a[row][column])
 {
     int i, j;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < row; i++)
     {
-        for (j = 0; j < m; j++)
+        for (j = 0; j < column; j++)
         {
             printf("%4d", a[i][j]);
         }
@@ -32,11 +31,11 @@ void print (int n, int m, int a[row][column])
     printf("\n");
 }
 
-void task1 (int field[row][column], int map[row][column])
+void task1 (int m, int n, int field[m][n], int map[m][n])
 {
     int i, j, value = 1;
     
-    for (j = 0; j < column; j ++)
+    for (j = 0; j < n; j ++)
     {
         if (map[0][j] == 0)
         {
@@ -46,7 +45,7 @@ void task1 (int field[row][column], int map[row][column])
     }
     
     value = 1;
-    for (i = 1; i < row; i ++)
+    for (i = 1; i < m; i ++)
     {
         if (map[i][0] == 0)
         {
@@ -54,7 +53,7 @@ void task1 (int field[row][column], int map[row][column])
         }
         field[i][0] = value;
         
-        for (j = 1; j < column; j ++)
+        for (j = 1; j < n; j ++)
         {
             if (map[i][j] == 0)
             {
@@ -70,19 +69,75 @@ void task1 (int field[row][column], int map[row][column])
 }
 
 
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+void lcs(char *X, char *Y, int m, int n)
+{
+    int L[m+1][n+1];
+    
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+            else if (X[i-1] == Y[j-1])
+                L[i][j] = L[i-1][j-1] + 1;
+            else
+                L[i][j] = max(L[i-1][j], L[i][j-1]);
+        }
+    }
+    
+    int index = L[m][n];
+    
+    char lcs[index+1];
+    
+    int i = m, j = n;
+    
+    while (i > 0 && j > 0)
+    {
+        if (X[i-1] == Y[j-1])
+        {
+            lcs[index-1] = X[i-1];
+            i--; j--; index--;
+        }
+        else if (L[i-1][j] > L[i][j-1])
+            i--;
+        else
+            j--;
+    }
+    
+    print(m + 1, n + 1, L);
+    printf("LCS: %s \n", lcs);
+}
+
+
 int main (int argc , char * argv [])
 {
+    int row = 5;
+    int column = 6;
     int A[row][column];
-    int MAP[row][column] = {
-        {1, 1, 0, 1, 1},
-        {1, 1, 1, 1, 1},
-        {0, 1, 0, 1, 0},
-        {1, 1, 1, 1, 1},
-        {1, 1, 0, 1, 1},
+    int MAP[5][6] = {
+        {1, 1, 0, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 1, 0, 1},
+        {1, 1, 1, 1, 1, 1},
+        {1, 1, 0, 1, 1, 1},
     };
     
-    task1(A, MAP);
+    task1(row, column, A, MAP);
     print(row, column, A);
+    
+    
+    char X[] = "GEEKMINDS";
+    char Y[] = "GEEKBRAINS";
+    int m = strlen(X);
+    int n = strlen(Y);
+    lcs(X, Y, m, n);
     
     return 0;
 }
+
