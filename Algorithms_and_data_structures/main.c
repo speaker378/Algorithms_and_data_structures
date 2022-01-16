34320393f507a05004dfcba7abd17c8e9e946ab9
speaker378 <speaker378debua@gmail.com>
2022-01-16 19:53:53 +0500

    Task 1

--- Algorithms_and_data_structures/main.c
+++ Algorithms_and_data_structures/main.c
@@ -4,141 +4,90 @@
 //
 //  Created by Сергей Черных on 15.12.2021.
 //
-
-//  1. Реализовать простейшую хэш-функцию. На вход функции подается строка, на выходе сумма кодов символов.
-//  2. Переписать программу, реализующее двоичное дерево поиска.
-//      а) Добавить в него обход дерева различными способами;
-//      б) Реализовать поиск в двоичном дереве поиска;
-//      в) *Добавить в программу обработку командной строки с помощью которой можно указывать из какого файла считывать данные, каким образом обходить дерево.
-//  3. *Разработать базу данных студентов из двух полей “Имя”, “Возраст”, “Табельный номер” в которой использовать все знания, полученные на уроках.
-//  Считайте данные в двоичное дерево поиска. Реализуйте поиск по какому-нибудь полю базы данных (возраст, вес)
+//  1. Реализовать сортировку подсчетом.
+//
+//  2. Реализовать быструю сортировку.
+//
+//  3. *Реализовать сортировку слиянием.
+//
+//  4. **Реализовать алгоритм сортировки со списком.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-int hash(char *string)
-{
-    int result = 0;
-    while(*string)
-    {
-        result += *string;
-        string++;
-    }
-    return result;
+struct timeval tv1 , tv2 , dtv;
+struct timezone tz;
+
+void time_start(void) {
+    gettimeofday (&tv1, &tz);
 }
 
+long time_stop(void) {
+    gettimeofday (&tv2, &tz);
+    dtv.tv_sec = tv2.tv_sec - tv1.tv_sec;
+    dtv.tv_usec = tv2.tv_usec - tv1.tv_usec;
+    if (dtv.tv_usec < 0) {
+        dtv.tv_sec--;
+        dtv.tv_usec += 1000000;
+    }
+    return dtv.tv_sec * 1000 + dtv.tv_usec / 1000;
+}
 
-typedef int T;
-typedef struct Node
-{
-    T data;
-    struct Node *left;
-    struct Node *right;
-    struct Node *parent;
-} Node;
+void swap (int *a, int *b) {
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *b ^ *a;
+}
 
-void printTree(Node *root) {
-    if (root)
-    {
-        printf("%d", root->data);
-        if (root->left || root->right)
-        {
-            printf("(");
-            if (root->left)
-                printTree(root->left);
-            else
-                printf("NULL");
-            printf(",");
-            if (root->right)
-                printTree(root->right);
-            else
-                printf("NULL");
-            printf(")");
+void print (int size, int* array) {
+    int i;
+    for (i = 0; i < size; i ++) {
+        printf("%i ", array[i]);
+    }
+    printf("\n");
+}
+
+void createRandomArray (int size, int* array) {
+    srand( (unsigned int) time(NULL) );
+    for (int i = 0; i < size; i++) {
+        array[i] = rand()%size;
+    }
+}
+
+void countingSort(int size, int array[size]) {
+    int i, j;
+    int *frequencyArray = (int*)malloc(size * sizeof(int));
+    
+    for (i = 0; i < size; i++) {
+        frequencyArray[i] = 0;
+    }
+    
+    int value = 0;
+    for (i = 0; i < size; i++) {
+        value = array[i];
+        frequencyArray[value]++;
+    }
+    
+    int index = 0;
+    for (j = 0; j < size; j++) {
+        for (i = 0; i < frequencyArray[j]; i++) {
+            array[index] = j;
+            index++;
         }
     }
+    free(frequencyArray);
 }
 
-Node* createNode(T value, Node *parent)
-{
-    Node* temp = (Node*)malloc(sizeof(Node));
-    temp->left = temp->right = NULL;
-    temp->data = value;
-    temp->parent = parent;
-    return temp;
-}
 
-void insertNode(Node **head, int value) {
-    if (*head == NULL)
-        *head = createNode(value, NULL);
-    else if (value > (*head)->data) {
-        if ((*head)->right)
-            insertNode(&(*head)->right, value);
-        else
-            (*head)->right = createNode(value, *head);
-    } else  {
-        if ((*head)->left)
-            insertNode(&(*head)->left, value);
-        else
-            (*head)->left = createNode(value, *head);
-    }
-}
-
-void preorderTravers(Node *root) {
-    if (root) {
-        printf("%i ", root->data);
-        preorderTravers(root->left);
-        preorderTravers(root->right);
-    }
-}
-
-void postorderTravers(Node *root) {
-    if (root) {
-        postorderTravers(root->left);
-        postorderTravers(root->right);
-        printf("%i ", root->data);
-    }
-}
-
-void inorderTravers(Node *root) {
-    if (root) {
-        inorderTravers(root->left);
-        printf("%i ", root->data);
-        inorderTravers(root->right);
-    }
-}
-
-T search(Node *root, T value)
-{
-    if (root == NULL)
-        return -1;
-    else if (root->data == value)
-        return value;
-    else if (root->data <= value)
-        return search(root->right, value);
-    else
-        return search(root->left, value);
-}
-
-int main ()
-{
-    int Hash;
-    Hash = hash("Test Text");
-    printf("%i \n", Hash);
-
-    int data[10] = {12, 3, 1, 10, 222, 4, 18, 11, 120, 3};
-    Node *root = NULL;
-    for (int i = 0; i <= 9; ++i) {
-        insertNode(&root, data[i]);
-    }
-    printTree(root);
-    printf("\n");
-    preorderTravers(root);
-    printf("\n");
-    postorderTravers(root);
-    printf("\n");
-    inorderTravers(root);
-    printf("\n");
-    printf("%i", search(root, 10));
-
+int main () {
+    int size = 100;
+    int data [size];
+    
+    createRandomArray(size, data);
+    print(size, data);
+    countingSort(size, data);
+    print(size, data);
+    
     return 0;
 }
